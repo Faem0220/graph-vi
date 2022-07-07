42c347fa73201584513abf7f7d370890ec6bacbf
Faem0220 <0faem1@gmail.com>
2022-07-07 15:55:42 -0300

    test node and edge data

--- src/index.js
+++ src/index.js
@@ -2,45 +2,64 @@ import "./styles.css";
 import cytoscape from "cytoscape";
 
 var cy = cytoscape({
+  container: document.getElementById("cy"), // container to render in
 
-  container: document.getElementById('cy'), // container to render in
-
-  elements: [ // list of graph elements to start with
-    { // node a
-      data: { id: 'a' }
+  elements: [
+    // list of graph elements to start with
+    {
+      // node a
+      data: { id: "a", address: "0x5C9a" }
     },
-    { // node b
-      data: { id: 'b' }
+    {
+      // node b
+      data: { id: "b", address: "0x81adf" }
     },
-    { // edge ab
-      data: { id: 'ba', source: 'a', target: 'b' }
+    {
+      // node a
+      data: { id: "c", address: "0x23d2a" },
+      style: {
+        width: 70,
+        height: 70
+      }
+    },
+    {
+      // node b
+      data: { id: "d", address: "0x2260" }
+    },
+    {
+      // edge ab
+      data: { id: "ba", source: "a", target: "b", weight: 0.2 }
+    },
+    {
+      // edge ab
+      data: { id: "ab", source: "a", target: "c" }
     }
   ],
 
-  style: [ // the stylesheet for the graph
+  style: [
+    // the stylesheet for the graph
     {
-      selector: 'node',
+      selector: "node",
       style: {
-        'background-color': '#666',
-        'label': 'data(id)'
+        "background-color": "#266",
+        label: "data(address)"
       }
     },
 
     {
-      selector: 'edge',
+      selector: "edge",
       style: {
-        'width': 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle',
-        'curve-style': 'bezier'
+        width: 3,
+        "line-color": "#ccc",
+        "target-arrow-color": "#ccc",
+        "target-arrow-shape": "triangle",
+        "curve-style": "bezier",
+        label: "data(weight)"
       }
     }
   ],
 
   layout: {
-    name: 'grid',
-    rows: 1
+    name: "random",
   }
-
 });
