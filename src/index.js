a45b918f89566afecbbdd9c6adc04640c721046c
Faem0220 <0faem1@gmail.com>
2022-07-10 14:01:58 -0300

    add graph from json

--- src/index.js
+++ src/index.js
@@ -1,65 +1,39 @@
 import "./styles.css";
 import cytoscape from "cytoscape";
 
+const graphData = require("./graph.json");
 var cy = cytoscape({
   container: document.getElementById("cy"), // container to render in
 
-  elements: [
-    // list of graph elements to start with
-    {
-      // node a
-      data: { id: "a", address: "0x5C9a" }
-    },
-    {
-      // node b
-      data: { id: "b", address: "0x81adf" }
-    },
-    {
-      // node a
-      data: { id: "c", address: "0x23d2a" },
-      style: {
-        width: 70,
-        height: 70
-      }
-    },
-    {
-      // node b
-      data: { id: "d", address: "0x2260" }
-    },
-    {
-      // edge ab
-      data: { id: "ba", source: "a", target: "b", weight: 0.2 }
-    },
-    {
-      // edge ab
-      data: { id: "ab", source: "a", target: "c" }
-    }
-  ],
+  elements: graphData,
 
   style: [
     // the stylesheet for the graph
     {
       selector: "node",
       style: {
-        "background-color": "#266",
-        label: "data(address)"
+        "background-color": "#3af",
+        label: "data(id)",
+        "font-family": "monospace"
       }
     },
 
     {
       selector: "edge",
       style: {
-        width: 3,
+        width: 2,
         "line-color": "#ccc",
-        "target-arrow-color": "#ccc",
+        "target-arrow-color": "#922",
         "target-arrow-shape": "triangle",
         "curve-style": "bezier",
+        "font-family": "monospace",
+        color: "#822",
         label: "data(weight)"
       }
     }
   ],
 
   layout: {
-    name: "random",
+    name: "breadthfirst"
   }
 });
